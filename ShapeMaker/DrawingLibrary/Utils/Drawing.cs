9f8631f491dc878265eaafa5a31f836132bf9032
Rowland Corentin <rowland@kinesistech.co.uk>
2017-02-28 19:22:34 +0000

    Add Drawing util to create a drawing which holds multiple widgets and prints them to the screen.  Implement within Main method of ShapeMaker project.

---
+++ ShapeMaker/DrawingLibrary/Utils/Drawing.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawingLibrary.Utils
+{
+    /// <summary>
+    /// Creates a drawing object that can contain multiple widgets and print them to the screen.
+    /// </summary>
+    public class Drawing
+    {
+        private List<Widget> _widgets { get; set; }
+
+
+        public Drawing()
+        {
+            _widgets = new List<Widget>(); 
+        }
+
+        public void AddWidget(Widget widget)
+        {
+            this._widgets.Add(widget);
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Current Drawing");
+            Console.WriteLine("---------------------");
+            foreach (var widget in _widgets)
+            {
+                Console.WriteLine(widget.Print());
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to end");
+            Console.ReadKey();
+        }
+
+    }
+}
--- ShapeMaker/ShapeMaker/Program.cs
+++ ShapeMaker/ShapeMaker/Program.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DrawingLibrary;
 
 namespace ShapeMaker
 {
     class Program
     {
-
-
         static void Main(string[] args)
         {
+            DrawingLibrary.Utils.Drawing drawing = new DrawingLibrary.Utils.Drawing();
 
+            drawing.AddWidget(new Rectangle(10, 10, 30, 40));
+            drawing.AddWidget(new Square(15, 30, 35));
+            drawing.AddWidget(new Ellipse(100, 150, 300, 200));
+            drawing.AddWidget(new Circle(1, 1, 300));
+            drawing.AddWidget(new Textbox(5, 5, 200, 100, "sample text"));
+
+            drawing.Print();
         }
-
-
     }
 }
