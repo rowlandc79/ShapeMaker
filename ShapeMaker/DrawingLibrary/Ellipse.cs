c8a9cbcff7cbd28f70c0cfdd44a9c7619467d739
Rowland Corentin <rowland@kinesistech.co.uk>
2017-02-28 19:05:31 +0000

    Ellipse test added, implemented & passed

---
+++ ShapeMaker/DrawingLibrary/Ellipse.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace DrawingLibrary
+{
+    public class Ellipse : Widget
+    {
+        private int _hDiameter { get; set; }
+        private int _vDiameter { get; set; }
+
+
+
+        public Ellipse(int x, int y, int h, int v)
+        {
+            base._location = new Location(x, y);
+            _hDiameter = h;
+            _vDiameter = v;
+        }
+
+        public override string Print()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("Ellipse");
+            sb.Append(" (");
+            sb.Append(_location.X.ToString());
+            sb.Append(",");
+            sb.Append(_location.Y.ToString());
+            sb.Append(") diameterH=");
+            sb.Append(_hDiameter.ToString());
+            sb.Append(" diameterV=");
+            sb.Append(_vDiameter.ToString());
+
+            return sb.ToString();
+        }
+
+
+    }
+}
--- ShapeMaker/ShapeTester/UnitTests.cs
+++ ShapeMaker/ShapeTester/UnitTests.cs
@@ -12,7 +12,6 @@ namespace ShapeTester
         public void TestBasicShape_Rectangle()
         {
             Widget widget = new Rectangle(10,10,30,40);
-
             Assert.AreEqual("Rectangle (10,10) width=30 height=40",widget.Print());
         }
 
@@ -20,8 +19,14 @@ namespace ShapeTester
         public void TestBasicShape_Square()
         {
             Widget widget = new Square(15, 30, 35);
-
             Assert.AreEqual("Square (15,30) size=35", widget.Print());
+        }
+
+        [TestMethod]
+        public void TestBasicShape_Ellipse()
+        {
+            Widget widget = new Ellipse(100,150,300,200);
+            Assert.AreEqual("Ellipse (100,150) diameterH=300 diameterV=200", widget.Print());
         }
 
     }
